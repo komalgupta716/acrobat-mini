21cef9790d306b3384cc54f195eb00d876291936
Komal Gupta <komgupta@adobe.com>
2021-01-27 23:00:10 +0530

    trying to make dynamic file load work

--- public/viewer.js
+++ public/viewer.js
@@ -119,10 +119,11 @@ function processUser()
     {
         var temp = parameters[0].split("=");
         l = unescape(temp[1]);
-        document.getElementById("embedView").data = l;
+        document.getElementById("embedView").src = l;
         document.getElementById("objectView").data = l;
         document.getElementById("fileLink").href = l;
-        document.getElementById("text").innerHTML = l;
+        // const myLink = liff.permanentLink.createUrl()
+        // document.getElementById("text").innerHTML = toString(myLink);
     }
     // document.getElementById("embedView").src = l;
   }
